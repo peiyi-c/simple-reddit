4118bd1fdb7a7ef2a4f251b2e207860a261d04a6
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-07 22:33:39 +0100

    add aside markups

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,7 +1,17 @@
 import "./index.scss";
-
+import logo from "../../assets/awesome-hamster.png";
+import { Subreddit } from "../Subreddit";
 export const Aside = () => {
-  return <aside className="center">Aside</aside>;
+  return (
+    <aside className="center container-sm">
+      <div className="aside">
+        <img className="aside__logo" src={logo} alt="awesome-hamster" />
+        <ul className="aside__subreddits" role="list">
+          <Subreddit />
+        </ul>
+      </div>
+    </aside>
+  );
 };
 
 export default Aside;
