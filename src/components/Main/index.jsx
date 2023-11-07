4c627c80f539352f59f31fcb3515d58f7dccab1c
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-07 22:34:12 +0100

    add main markup

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,7 +1,21 @@
 import "./index.scss";
+import Card from "../Card";
 
 export const Main = () => {
-  return <main className="center">Main</main>;
+  return (
+    <main className="center container-sm">
+      <section className="filter container-sm">
+        <ion-icon name="star"></ion-icon>
+        <ion-icon name="flame"></ion-icon>
+        <ion-icon name="trending-up"></ion-icon>
+      </section>
+      <section className="cards">
+        <Card />
+        <Card />
+        <Card />
+      </section>
+    </main>
+  );
 };
 
 export default Main;
