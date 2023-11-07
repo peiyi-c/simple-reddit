59012e670211cbce7ed815aa7fcac06a576ec595
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-07 20:15:07 +0100

    add navigation markup and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 const App = () => {
   return (
     <>
       <Header />
+      <Navigation />
     </>
   );
 };
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,7 +1,24 @@
 import "./index.scss";
 
 export const Navigation = () => {
-  return <nav>Navigation</nav>;
+  return (
+    <nav>
+      <ul role="list" className="navi">
+        <li>
+          <button>POSTS</button>
+        </li>
+        <li>
+          <button>COMMENTS</button>
+        </li>
+        <li>
+          <button>COMMUNITIES</button>
+        </li>
+        <li>
+          <button>PEOPLE</button>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
