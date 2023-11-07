73329c7fb88e5537519faa550070e06b0108e9e7
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-07 22:34:53 +0100

    add card markup

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,19 @@
 import "./index.scss";
 
-import "./index.scss";
-
 export const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="card">
+      <div className="card-left">vote</div>
+      <div className="card-right">
+        <h1 className="card__title">Title</h1>
+        <img className="card__image" src="" alt="card" />
+        <div className="card-bottom">
+          <span className="card__time">time</span>
+          <ion-icon name="chatbubbles"></ion-icon>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
