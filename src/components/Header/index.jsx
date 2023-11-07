9eb6a930119564509248ec80fdb77bcb20ec6561
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-07 19:02:40 +0100

    add header markup and style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,26 @@
 import "./index.scss";
+import logo from "../../assets/logo-no-background.svg";
 
 export const Header = () => {
-  return <header className="header"></header>;
+  return (
+    <header className="center">
+      <div className="header container container-sm center">
+        <img className="header__logo" src={logo} alt="awesome-hamster" />
+
+        <fieldset>
+          <ion-icon name="search" title="search"></ion-icon>
+          <label htmlFor="search" />
+          <input id="search" type="search" name="search" />
+        </fieldset>
+
+        <ion-icon name="heart" title="popular"></ion-icon>
+
+        <ion-icon name="balloon" title="all"></ion-icon>
+
+        <button role="switch">Dark Mode</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
