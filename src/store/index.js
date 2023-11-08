c98340f3d33ed8af298608c4399fff439b3a56cf
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-08 20:44:48 +0100

    import searchReducer, subredditReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import themeReducer from "../features/themeSlice.js";
+import searchReducer from "../features/searchTermSlice.js";
+import subredditReducer from "../features/subredditSlice.js";
 
 export default configureStore({
   reducer: combineReducers({
     theme: themeReducer,
+    search: searchReducer,
+    subreddit: subredditReducer,
   }),
 });
