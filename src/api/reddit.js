bc0dc9ae69185ef85a64281eccd1ecc26694116f
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-08 18:12:14 +0100

    add async functions

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -1,6 +1,33 @@
 export const BASE_URL = "https://www.reddit.com";
-export const getPosts = async (subreddit) => {
-  const response = await fetch(`${BASE_URL}${subreddit}.json`);
-  const json = await response.json();
-  return json.data.children;
+
+export const getSubreddit = async (subreddit) => {
+  try {
+    const response = await fetch(`${BASE_URL}${subreddit}.json`);
+    const json = await response.json();
+    return json.data.children;
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+export const getSearchContent = async (searchTerm, type = "link") => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/search.json?q=${searchTerm}&type=${type}`
+    );
+    const json = await response.json();
+    return json.data.children;
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+export const getPostComments = async (permalink) => {
+  try {
+    const response = await fetch(`${BASE_URL}${permalink}.json`);
+    const json = await response.json();
+    return json[1].data.children.map((subreddit) => subreddit.data);
+  } catch (err) {
+    console.warn(err);
+  }
 };
