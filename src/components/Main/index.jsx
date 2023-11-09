5f9f03eb323ff51ec8f2405c7dd46081f0f6a0d3
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-09 16:06:32 +0100

    add post markup

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,13 +1,32 @@
+/* eslint-disable no-unused-vars */
 import "./index.scss";
 import Post from "../Post";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectPosts,
+  fetchPosts,
+  fetchComments,
+} from "../../features/subredditSlice";
+import { useEffect } from "react";
 
 export const Main = () => {
+  const redditPosts = useSelector((state) => state.subreddit);
+  const { hasError, isLoading, selectedSubreddit } = redditPosts;
+  const posts = useSelector(selectPosts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts(selectedSubreddit));
+  }, [dispatch, selectedSubreddit]);
+
+  console.log(posts);
+
   return (
     <main className="center container-sm">
       <section className="cards">
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((post, index) => (
+          <Post key={post.id} post={post} />
+        ))}
       </section>
     </main>
   );
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,18 +1,44 @@
+/* eslint-disable react/prop-types */
 import "./index.scss";
-
-export const Post = () => {
+import moment from "moment";
+export const Post = ({ post }) => {
+  const {
+    id,
+    ups,
+    downs,
+    title,
+    url,
+    author,
+    subreddit_name_prefixed,
+    created_utc,
+    showingComments,
+    permlink,
+    num_comments,
+  } = post.data;
   return (
-    <div className="post">
-      <div className="post-left">vote</div>
+    <article className="post" key={id}>
+      <div className="post-left">
+        <ion-icon name="arrow-up-outline"></ion-icon>
+        <span>{ups}</span>
+        <ion-icon name="arrow-down-outline"></ion-icon>
+        <span>{downs}</span>
+      </div>
       <div className="post-right">
-        <h1 className="post__title">Title</h1>
-        <img className="post__image" src="" alt="post" />
+        <span className="post__subreddit-name">{subreddit_name_prefixed}</span>
+        <span className="post__author">
+          Posted by {author} {moment.unix(created_utc).fromNow()}
+        </span>
+
+        <h1 className="post__title">{title}</h1>
+        {url.includes(".jpg") && (
+          <img className="post__image" src={url} alt="post" />
+        )}
         <div className="post-bottom">
-          <span className="post__time">time</span>
           <ion-icon name="chatbubbles"></ion-icon>
+          <span>{num_comments}</span>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
