2cd26457f4bc66bf36a0f2b74f6a58a868ee8c9a
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-21 13:00:30 +0100

    add aside markup and styles

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,13 +1,37 @@
 import "./index.scss";
 import logo from "../../assets/awesome-hamster.png";
 import { Subreddit } from "../Subreddit";
+import {
+  fetchHamsterReddits,
+  selectCommunities,
+} from "../../features/hamsterRedditsSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 export const Aside = () => {
+  const dispatch = useDispatch();
+  const communities = useSelector(selectCommunities);
+
+  useEffect(() => {
+    dispatch(fetchHamsterReddits());
+  }, [dispatch]);
+
   return (
-    <aside className="center container-sm">
+    <aside className="center">
       <div className="aside">
-        <img className="aside__logo" src={logo} alt="awesome-hamster" />
+        <div className="aside__heading">
+          <img
+            className="aside__heading-logo"
+            src={logo}
+            alt="awesome-hamster"
+          />
+          <h2>Say heke</h2>
+        </div>
+
         <ul className="aside__subreddits" role="list">
-          <Subreddit />
+          {communities.map((community, index) => (
+            <Subreddit community={community} key={index} />
+          ))}
         </ul>
       </div>
     </aside>
