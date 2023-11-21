505be75eace9d29c641bae5d20c4cf2ae869ae9e
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-21 13:03:15 +0100

    add DummyImage

--- src/components/Community/index.jsx
+++ src/components/Community/index.jsx
@@ -1,19 +1,40 @@
 /* eslint-disable react/prop-types */
 import "./index.scss";
 import moment from "moment";
+import { imageFilter } from "../../utilities/helpers";
+import { DummyImage } from "../DummyImage";
+
 export const Community = ({ community }) => {
-  const { subscribers, display_name_prefixed, public_description, created } =
-    community.data;
+  const {
+    subscribers,
+    display_name_prefixed,
+    public_description,
+    created,
+    community_icon,
+    name,
+  } = community.data;
+
   return (
     <div className="community-wrapper">
       <div className="community-top">
-        <h1>{display_name_prefixed}</h1>
+        <div>
+          {community_icon ? (
+            <img
+              className="community-image"
+              src={imageFilter(community_icon)}
+              alt={name}
+            />
+          ) : (
+            <DummyImage size="2rem" />
+          )}
+          <h1>{display_name_prefixed}</h1>
+        </div>
         <small>Created {moment.unix(created).fromNow()}</small>
       </div>
       <div className="community-bottom">
-        <span>{public_description}</span>
+        <span className="description">{public_description}</span>
         <br />
-        <span>subscribers: {subscribers}</span>
+        <span className="subscribers">subscribers: {subscribers}</span>
       </div>
     </div>
   );
