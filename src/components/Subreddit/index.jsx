2d2f3504d186f75edd5301a584f97b39c20fd303
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-21 13:02:50 +0100

    add dummyImage

--- src/components/Subreddit/index.jsx
+++ src/components/Subreddit/index.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/prop-types */
 import "./index.scss";
-
-export const Subreddit = () => {
+import { imageFilter } from "../../utilities/helpers";
+import { DummyImage } from "../DummyImage";
+export const Subreddit = ({ community }) => {
+  const { display_name_prefixed, community_icon, name } = community.data;
   return (
     <li>
-      <img src="" alt="subreddit" />
-      <span>name</span>
+      {community_icon ? (
+        <img src={imageFilter(community_icon)} alt={name} />
+      ) : (
+        <DummyImage size="1.85rem" />
+      )}
+      <span>{display_name_prefixed}</span>
     </li>
   );
 };
--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
 import "./index.scss";
+import { imageFilter } from "../../utilities/helpers";
+import { DummyImage } from "../DummyImage";
 
 export const User = ({ user }) => {
   const { icon_img, comment_karma, name } = user.data;
-  const imageFilter = (icon) => {
-    const index = icon.indexOf("?");
-    return icon.slice(0, index + 1);
-  };
+
   return (
     <div className="user-wrapper">
-      <img className="user-image" src={imageFilter(icon_img)} alt={name} />
-      <div>
+      {icon_img ? (
+        <img
+          className="left user-image"
+          src={imageFilter(icon_img)}
+          alt={name}
+        />
+      ) : (
+        <DummyImage size="1.85rem" />
+      )}
+      <div className="right">
         <h3 className="user-name">{name}</h3>
         <small className="user-comment">Comments Karmar {comment_karma}</small>
       </div>
