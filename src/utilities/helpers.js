ee36db30f0d0d4d166d5a49a8ec7b517ad0ca20c
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-21 13:00:51 +0100

    add randomColor function

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -5,3 +5,28 @@ export const imageFilter = (icon) => {
   }
   return icon;
 };
+
+export const randomColor = () => {
+  const hex = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[Math.floor(Math.random() * hex.length + 1)];
+  }
+  return hexColor;
+};
