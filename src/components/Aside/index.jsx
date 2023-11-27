3d199c0929c8234e72e2adaca916f04b9109e168
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-27 22:09:14 +0100

    add link, loading skeleton

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,16 +1,23 @@
 import "./index.scss";
-import logo from "../../assets/awesome-hamster.png";
+import logoLight from "../../assets/awesome-hamster.png";
+import logoDark from "../../assets/awesome-hamster-yellow.png";
+
 import { Subreddit } from "../Subreddit";
-import {
-  fetchHamsterReddits,
-  selectCommunities,
-} from "../../features/hamsterRedditsSlice";
+import { fetchHamsterReddits } from "../../features/hamsterRedditsSlice";
+import { selectTheme } from "../../features/themeSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { SubredditLoading } from "../SubredditLoading";
+import { randomNumber } from "../../utilities/helpers";
+import { DummyImage } from "../DummyImage";
 
 export const Aside = () => {
   const dispatch = useDispatch();
-  const communities = useSelector(selectCommunities);
+
+  const { communities, hasError, isLoading } = useSelector(
+    (state) => state.hamsterReddits
+  );
+  const theme = useSelector(selectTheme);
 
   useEffect(() => {
     dispatch(fetchHamsterReddits());
@@ -22,16 +29,38 @@ export const Aside = () => {
         <div className="aside__heading">
           <img
             className="aside__heading-logo"
-            src={logo}
+            src={theme === "light" ? `${logoLight}` : `${logoDark}`}
             alt="awesome-hamster"
           />
+
           <h2>Say heke</h2>
+          <img
+            className="aside__heading-logo"
+            src={theme === "light" ? `${logoLight}` : `${logoDark}`}
+            alt="awesome-hamster"
+          />
         </div>
 
         <ul className="aside__subreddits" role="list">
-          {communities.map((community, index) => (
-            <Subreddit community={community} key={index} />
-          ))}
+          {isLoading &&
+            Array(randomNumber(10))
+              .fill(0)
+              .map((num, index) => <SubredditLoading key={index} />)}
+          {hasError && (
+            <>
+              <li>
+                <DummyImage size="1.85rem" />
+                <span>Unexpected error happens</span>
+              </li>
+              <li>
+                <DummyImage size="1.85rem" /> <span>, please try it later</span>
+              </li>
+            </>
+          )}
+          {!isLoading &&
+            communities.map((community, index) => (
+              <Subreddit community={community} key={index} />
+            ))}
         </ul>
       </div>
     </aside>
