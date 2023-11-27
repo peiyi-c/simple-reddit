6ad273bed23086b2563b8cd062cecc66e5be567d
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-27 22:10:37 +0100

    add link

--- src/components/Subreddit/index.jsx
+++ src/components/Subreddit/index.jsx
@@ -2,10 +2,27 @@
 import "./index.scss";
 import { imageFilter } from "../../utilities/helpers";
 import { DummyImage } from "../DummyImage";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setSelectedSubreddit } from "../../features/subredditSlice";
+import { viewPosts } from "../../features/visibilitySlice";
+
 export const Subreddit = ({ community }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { display_name_prefixed, community_icon, name } = community.data;
+
+  // Change Subreddit
+  const handleRedditChange = (e) => {
+    const subreddit = "/" + e.target.innerText;
+    dispatch(setSelectedSubreddit(subreddit));
+    dispatch(viewPosts());
+    navigate(`${subreddit}`);
+  };
+
   return (
-    <li>
+    <li onClick={handleRedditChange}>
       {community_icon ? (
         <img src={imageFilter(community_icon)} alt={name} />
       ) : (
