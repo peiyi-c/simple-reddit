b4f70bf4b843ea751ffdf043a129f12f466c61d1
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-02 09:40:20 +0100

    setup toggle sub replies

--- src/components/Reply/index.jsx
+++ src/components/Reply/index.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./index.scss";
 import moment from "moment";
 
 export const Reply = ({ reply }) => {
+  const [toggleOpen, setToggleOpen] = useState(false);
+  const replyReplies = reply?.replies?.data?.children[0] || "";
+
   return (
     <div className="reply">
-      {reply?.author_fullname ? (
+      {reply?.author_fullname && reply?.author_fullname !== "[deleted]" ? (
         <div className="reply-wrapper">
           <div className="reply-heading">
             <span className="reply__author">
@@ -16,6 +20,17 @@ export const Reply = ({ reply }) => {
             </span>
           </div>
           <span className="reply-body">{reply?.body && reply.body}</span>
+          {replyReplies && replyReplies !== "" && (
+            <>
+              <div
+                className="reply-reply"
+                onClick={() => setToggleOpen((prev) => !prev)}
+              >
+                <ion-icon name="chatbubbles"></ion-icon>
+              </div>
+              {toggleOpen && <Reply reply={replyReplies.data} />}
+            </>
+          )}
         </div>
       ) : (
         ""
